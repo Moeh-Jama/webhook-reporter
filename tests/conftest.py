04bdf58f297419fb20751798c713be4e89a111cc
Moeh-Jama <moeh.jama@gmail.com>
2024-09-22 14:54:21 +0100

    Add github action env fields for tests
    the tests fail for 'pytest' as the environment variables are not set.
    currently we set random Github Action env fields to keep them passing
    
    In the normal circumstance these fields are generated during the
    workflow run

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,4 +4,8 @@ def pytest_generate_tests(metafunc):
     """Set all environments for tests"""
     os.environ['ICON_URL'] = "https://icon_url.com/icon.png"
     os.environ['BOT_IMAGE'] = "https://icon_url.com/bot_icon.png"
-    # os.environ['COVERAGE_THRESHOLD'] = "50"
+    os.environ['GITHUB_REPOSITORY'] = 'random-repo'
+    os.environ['GITHUB_SHA'] = 'RAN2223DOM'
+    os.environ['GITHUB_RUN_ID'] = "555225525"
+    os.environ['GITHUB_REF'] = 'refs/pull/38/merge'
+    os.environ['GITHUB_ACTOR_ID'] = "10217249"
